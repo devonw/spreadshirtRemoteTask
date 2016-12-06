b5de3cc156679949d8b115667a89f072a225cf01
Jakob Runge <sicarius@g4t3.de>
2016-12-06 22:30:03 +0100

    Improve DesignSearch till first query
    - Next step will be an implementation for paging.
      I think I can use generators for this.

--- src/DesignSearch.js
+++ src/DesignSearch.js
@@ -1,4 +1,5 @@
 import SearchStates from "./SearchStates";
+import axios from "../node_modules/axios/index";
 
 class DesignSearch {
   constructor(settings) {
@@ -6,9 +7,16 @@ class DesignSearch {
     this.keywords = "";
     this.searchState = SearchStates.pending;
     this.searchResults = []; // Filled when search succeeds
-    this.searchErrorMsg = ""; // Filled when search failes
+    this.searchErrorMsg = null; // Filled when search failes
     this.shopId = "205909"; // Given trough task description
+    this.offset = 0;
+    this.limit = 50;
     this.searchImmediately = true;
+    this.callback = null; // Where we say hello in case of search updates
+    //Bind this context:
+    this.searchSuccess = this.searchSuccess.bind(this);
+    this.searchError = this.searchError.bind(this);
+    this.notifyCallback = this.notifyCallback.bind(this);
     //Apply settings:
     Object.assign(this, settings || {});
     //Trigger search if desired:
@@ -22,9 +30,37 @@ class DesignSearch {
   }
 
   search() {
+    const config = {
+      params: {
+        query: this.keywords,
+        mediaType: "json",
+        spellcheck: true,
+        fullData: true,
+        offset: this.offset,
+        limit: this.limit
+      }
+    };
+    return axios.get(this.searchBaseUrl(), config)
+         .then(this.searchSuccess, this.searchError)
+         .then(this.notifyCallback);
+  }
+
+  searchSuccess(response) {
+    this.searchState = SearchStates.success;
+    //console.log("DEBUG searchSuccess", response);
     //FIXME IMPLEMENT
   }
 
+  searchError(error) {
+    this.searchState = SearchStates.fail;
+    this.searchErrorMsg = error;
+  }
+
+  notifyCallback() {
+    if(this.callback) {
+      this.callback(this);
+    }
+  }
 }
 
 export default DesignSearch;
--- src/DesignSearch.test.js
+++ src/DesignSearch.test.js
@@ -1,13 +1,42 @@
+/* global it:true expect: true */
 import DesignSearch from "./DesignSearch";
 import SearchStates from "./SearchStates";
 
-/* global it:true expect: true */
+const mkSimpleSearch = () => {
+  return new DesignSearch({
+    searchImmediately: false,
+    keywords: "Heart"
+  });
+};
+
 it("must have the shopId in its searchBaseUrl", () => {
-  const search = new DesignSearch({searchImmediately: false});
+  const search = mkSimpleSearch();
   expect(search.searchBaseUrl()).toMatch(/shops\/205909\/designs/);
 });
 
 it("must start with a pending search", () => {
-  const search = new DesignSearch({searchImmediately: false});
+  const search = mkSimpleSearch();
   expect(search.searchState).toBe(SearchStates.pending);
+});
+
+it("must use the callback after search is called", () => {
+  const search = mkSimpleSearch();
+  search.callback = () => {
+    expect(search.searchState).not.toBe(SearchStates.pending);
+  };
+  return search.search();
+});
+
+it("must set searchState to success after searchSuccess", () => {
+  const search = mkSimpleSearch();
+  search.searchSuccess();
+  expect(search.searchState).toBe(SearchStates.success);
+});
+
+it("must set searchState to fail after searchError", () => {
+  const search = mkSimpleSearch();
+  const errorMsg = "Failed because of test.";
+  search.searchError(errorMsg);
+  expect(search.searchState).toBe(SearchStates.fail);
+  expect(search.searchErrorMsg).toBe(errorMsg);
 });
