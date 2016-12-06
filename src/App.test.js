5cdd828e11fca7ffed22849a81a4ee434c0039f8
Jakob Runge <sicarius@g4t3.de>
2016-12-06 09:19:58 +0100

    Add eslint rules to App.test.js
    - Before eslint would complain about:
      `it` being undefined
      `App` and `React` being unused imports

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,9 @@
+/* eslint no-unused-vars: ["error", {"varsIgnorePattern": "React|App"}]*/
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+/* global it:true */
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<App />, div);
