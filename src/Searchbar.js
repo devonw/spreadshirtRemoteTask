9575aaa312d4ce7359e01a2fd7fae8919a6f22f2
Jakob Runge <sicarius@g4t3.de>
2016-12-06 13:45:10 +0100

    Add event handlers to Searchbar

--- src/Searchbar.js
+++ src/Searchbar.js
@@ -1,19 +1,39 @@
-/* eslint no-unused-vars: ["error", {"varsIgnorePattern": "React|Button|Form(Control|Group)|InputGroup"}]*/
+/* eslint no-unused-vars: ["error", {"varsIgnorePattern": "React|Button|Form(Control|Group)|InputGroup|Glyphicon"}]*/
 import React, { Component } from "react";
 import {Button,
         FormControl,
         FormGroup,
+        Glyphicon,
         InputGroup} from "react-bootstrap";
 
 class Searchbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {keywords: ""};
+
+    this.updateKeywords = this.updateKeywords.bind(this);
+    this.searchKeywords = this.searchKeywords.bind(this);
+  }
+
+  updateKeywords() {
+    this.setState({keywords: event.target.value});
+  }
+
+  searchKeywords() {
+    window.alert("SOMETIHNG HAPPENED!");
+  }
+
   render() {
     return (
       <FormGroup controlId="keywords">
         <InputGroup>
-          <FormControl type="text" placeholder="Keywords…" />
-          <InputGroup.Button>
+          <FormControl type="text"
+                       placeholder="Keywords…"
+                       value={this.state.keywords}
+                       onChange={this.updateKeywords} />
+          <InputGroup.Button onClick={this.searchKeywords}>
             <Button>
-            Search
+              <Glyphicon glyph="search"/>
             </Button>
           </InputGroup.Button>
         </InputGroup>
