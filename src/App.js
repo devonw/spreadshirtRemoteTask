4155a27394e6d693ac141ce268d20370ac6e9d06
Jakob Runge <sicarius@g4t3.de>
2016-12-07 19:57:37 +0100

    Let App use current search features
    - Next step will likely be building the voting system.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,78 @@
-/* eslint no-unused-vars: ["error", {"varsIgnorePattern": "React"}]*/
+/* eslint no-unused-vars: ["error", {"varsIgnorePattern": "React|Searchbar"}]*/
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Searchbar from "./Searchbar";
+import AppStates from "./AppStates";
+import DesignSearch from "./DesignSearch";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      appState: AppStates.search,
+      currentSearch: null,
+      searchHistory: new Map()
+    };
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  handleSearch() {
+    const keywords = this.refs.searchBar.keywords;
+    const searchHistory = this.state.searchHistory;
+    if(searchHistory.has(keywords)){
+      const search = searchHistory.get(keywords);
+
+      this.setState({
+        appState: AppStates.vote,
+        currentSearch: search
+      });
+    }else{
+      const search = new DesignSearch();
+      const history = new Map(searchHistory);
+      history.set(search.keywords, search);
+
+      this.setState({
+        appState: AppStates.vote,
+        currentSearch: search,
+        searchHistory: history
+      });
+    }
+  }
+
   render() {
+    const searchBar = this.refs.searchBar;
+
+    switch(this.state.appState){
+    case AppStates.search:
+      if(searchBar && searchBar.state.searchTriggered){
+        this.handleSearch();
+        return this.render();
+      }
+      return this.renderSearch();
+    case AppStates.vote:
+      // FIXME IMPLEMENT
+      return this.renderBase("Voting intensifies");
+    case AppStates.statistics:
+      // FIXME IMPLEMENT
+      return this.renderBase("Statistics intensifies");
+    default:
+      return this.renderSearch();
+    }
+  }
+
+  renderSearch() {
+    return this.renderBase(
+      <Searchbar ref="searchBar" onSearch={() => this.handleSearch()}/>
+    );
+  }
+
+  renderBase(content) {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>Spreadshirt Remote Task</h2>
         </div>
-        <Searchbar />
+        {content}
       </div>
     );
   }
