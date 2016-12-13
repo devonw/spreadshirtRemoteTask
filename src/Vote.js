0ebb012326d71e4b4959ebf07b443f5871c131d3
Jakob Runge <sicarius@g4t3.de>
2016-12-13 12:16:02 +0100

    Work on Vote.js

--- src/Vote.js
+++ src/Vote.js
@@ -1,13 +1,22 @@
 /* eslint no-unused-vars: ["error", {"varsIgnorePattern": "React"}]*/
 import React, { Component } from "react";
+import SearchStates from "./SearchStates";
 
 const exampleImage = "https://image.spreadshirtmedia.com/image-server/v1/designs/2104687";
 
 class Vote extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      designs: []
+    };
     //We expect props to contain `search`.
     this.searchEnded = this.searchEnded.bind(this);
+
+    if(props.search.searchState === SearchStates.pending){
+      props.search.callback = this.searchEnded;
+      props.search.search();
+    }
   }
 
   searchEnded() {
